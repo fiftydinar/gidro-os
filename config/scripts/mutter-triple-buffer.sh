fcac0ab2c7384b4870b8e3301bee947578314c38
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-09-27 08:42:57 +0200

    Revert triple buffer mutter

--- config/scripts/mutter-triple-buffer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-set -oue pipefail
-
-rpm-ostree cliwrap install-to-root / && \
-rpm-ostree override remove mutter --install=mutter
-
