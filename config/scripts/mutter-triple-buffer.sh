c0797032c094586e0ac1551844117fb55fa158f7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-09-29 00:30:51 +0200

    Revert "Triple buffer again"
    This reverts commit a23b434749cd2243a6c64c218bb04147037e0768.

--- config/scripts/mutter-triple-buffer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-rpm-ostree cliwrap install-to-root / && \
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:nxmbit:gnome-perf-patched mutter
