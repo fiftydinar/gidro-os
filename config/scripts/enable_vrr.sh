00d5bc6811db5e170fd93b1c42e0dcad4b1c5733
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-09-29 21:52:49 +0200

    Try changing order of the execution of VRR

--- config/scripts/enable_vrr.sh
+++ config/scripts/enable_vrr.sh
@@ -3,4 +3,3 @@ set -oue pipefail
 
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
 echo "MUTTER_DEBUG_FORCE_KMS_MODE=simple" >> /usr/etc/environment
-gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
