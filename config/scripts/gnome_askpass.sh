94482441d2a18e3139ce3d8ea15b65a574e69b85
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-10-01 16:16:41 +0200

    Set askpass in environment instead & use -A for sudo commands on post-setup

--- config/scripts/gnome_askpass.sh
+++ config/scripts/gnome_askpass.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-SUDO_ASKPASS='/usr/libexec/openssh/gnome-ssh-askpass'
-export SUDO_ASKPASS
+echo "SUDO_ASKPASS='/usr/libexec/openssh/gnome-ssh-askpass'" >> /usr/etc/environment
