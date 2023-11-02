e3df82f7a64c60bc878b811a780754c111d6ba29
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-02 21:53:08 +0100

    I forgot to include System76-Scheduler package
    I only included extension, which is not enough.

--- config/scripts/system76-scheduler.sh
+++ config/scripts/system76-scheduler.sh
@@ -4,5 +4,4 @@
 set -oue pipefail
 
 systemctl enable com.system76.Scheduler.service
-mkdir /usr/etc/system76-scheduler
 wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
