fb8b9c1a1d704f61396071955dbace87f0f00c8e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-02 13:12:25 +0100

    Use Bazzite's desktop System76-Scheduler config file instead

--- config/scripts/system76-scheduler.sh
+++ config/scripts/system76-scheduler.sh
@@ -5,6 +5,4 @@ set -oue pipefail
 
 systemctl enable com.system76.Scheduler.service
 mkdir /usr/etc/system76-scheduler
-wget https://raw.githubusercontent.com/pop-os/system76-scheduler/5c20d3dbbc4d0f2a324ecd536adb9131dfcaeaf0/data/config.kdl -O /usr/etc/system76-scheduler/config.kdl
-cd /usr/etc/system76-scheduler/
-sed -i 's/cfs-profiles enable=true/cfs-profiles enable=false/' config.kdl
+wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
