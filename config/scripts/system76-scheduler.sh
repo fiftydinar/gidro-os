f5ccafe51efb52d1cef46a15c0bf81890cd946d3
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-02 12:48:33 +0100

    Fix System76-Scheduler CFS change

--- config/scripts/system76-scheduler.sh
+++ config/scripts/system76-scheduler.sh
@@ -4,6 +4,7 @@
 set -oue pipefail
 
 systemctl enable com.system76.Scheduler.service
+mkdir /usr/etc/system76-scheduler
 wget https://raw.githubusercontent.com/pop-os/system76-scheduler/5c20d3dbbc4d0f2a324ecd536adb9131dfcaeaf0/data/config.kdl -O /usr/etc/system76-scheduler/config.kdl
 cd /usr/etc/system76-scheduler/
 sed -i 's/cfs-profiles enable=true/cfs-profiles enable=false/' config.kdl
