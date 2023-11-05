6e4afe90a375a7cf68587c0db52f1b583c5cf7f0
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-05 13:42:59 +0100

    Fix MoreWaita script

--- config/scripts/morewaita.sh
+++ config/scripts/morewaita.sh
@@ -3,8 +3,8 @@ set -oue pipefail
 
 mkdir /usr/share/icons/MoreWaita-main
 wget https://github.com/somepaulo/MoreWaita/archive/refs/heads/main.zip -O /usr/share/icons/MoreWaita-main/main.zip
-unzip main.zip -d /usr/share/icons
 cd /usr/share/icons/MoreWaita-main
+unzip main.zip -d /usr/share/icons
 find . \( -name "*.build" -o -name "*.sh" -o -name "*.md" -o -name "*.py" -o -name "*.git" \) -type f -delete
 rm -r main.zip _dev .github .gitignore
 mv /usr/share/icons/MoreWaita-main /usr/share/icons/MoreWaita
