a5d7f7e0f2523d01eab8f0be956f1bb7c568fcbd
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-05 13:47:04 +0100

    Change affected directory before it's name change

--- config/scripts/morewaita.sh
+++ config/scripts/morewaita.sh
@@ -7,6 +7,7 @@ cd /usr/share/icons/MoreWaita-main
 unzip main.zip -d /usr/share/icons
 find . \( -name "*.build" -o -name "*.sh" -o -name "*.md" -o -name "*.py" -o -name "*.git" \) -type f -delete
 rm -r main.zip _dev .github .gitignore
+cd /usr/share/icons
 mv /usr/share/icons/MoreWaita-main /usr/share/icons/MoreWaita
 gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita
 xdg-desktop-menu forceupdate
