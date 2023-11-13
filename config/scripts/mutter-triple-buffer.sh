5eba660db3cb792169259037fa2950361a059b91
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2023-11-13 09:57:48 +0100

    Switch to more updated Mutter triple buffering patch repo

--- config/scripts/mutter-triple-buffer.sh
+++ config/scripts/mutter-triple-buffer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:eliasofwaffle:mutter-dynamic-triplebuffer mutter mutter-common
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common
