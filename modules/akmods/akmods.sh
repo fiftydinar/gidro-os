d1680d9557b33edc7cef97acdd4b0966b939a90b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 14:30:50 +0100

    Update akmods.sh

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
-IMAGE_DEVICES=$(echo "${BASE_IMAGE}" | sed 's/asus-nvidia//' | grep -o "asus" || echo "${BASE_IMAGE}" | sed 's/surface-nvidia//' | grep -o "surface")
-
 get_yaml_array INSTALL '.install[]' "$1"
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
@@ -14,9 +11,4 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
 
-  if [[ "$IMAGE_DEVICES" == asus ]] || [ "$IMAGE_NVIDIA" == asus-nvidia ] || [ "$IMAGE_DEVICES" == surface ] || [ "$IMAGE_NVIDIA" == surface-nvidia ]; then
-    rpm-ostree install kernel-tools "$INSTALL_STR"
-    else
-    rpm-ostree install kernel-devel-matched "$INSTALL_STR"
-  fi  
-fi
+rpm-ostree install kernel-devel-matched "$INSTALL_STR"
