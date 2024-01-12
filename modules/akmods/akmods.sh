6541ccfebb46cef9945c06de079f70e6f0c14faa
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 14:55:32 +0100

    Revert "Update akmods.sh"
    This reverts commit d1680d9557b33edc7cef97acdd4b0966b939a90b.

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
+IMAGE_DEVICES=$(echo "${BASE_IMAGE}" | sed 's/asus-nvidia//' | grep -o "asus" || echo "${BASE_IMAGE}" | sed 's/surface-nvidia//' | grep -o "surface")
+
 get_yaml_array INSTALL '.install[]' "$1"
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
@@ -11,4 +14,9 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
 
-rpm-ostree install kernel-devel-matched "$INSTALL_STR"
+  if [[ "$IMAGE_DEVICES" == asus ]] || [ "$IMAGE_NVIDIA" == asus-nvidia ] || [ "$IMAGE_DEVICES" == surface ] || [ "$IMAGE_NVIDIA" == surface-nvidia ]; then
+    rpm-ostree install kernel-tools "$INSTALL_STR"
+    else
+    rpm-ostree install kernel-devel-matched "$INSTALL_STR"
+  fi  
+fi
