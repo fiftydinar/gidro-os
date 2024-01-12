6a5e85d936ff16c8c38f2878a8a6b6cf7dcc8697
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 15:52:54 +0100

    Revert "Update akmods.sh"
    This reverts commit be9801c0b186ff47c1361f7be01cdac406caf02d.

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -14,8 +14,8 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
   if [[ "$IMAGE_DEVICES" == asus ]] || [[ "$IMAGE_NVIDIA" == asus-nvidia ]] || [[ "$IMAGE_DEVICES" == surface ]] || [[ "$IMAGE_NVIDIA" == surface-nvidia ]]; then
-    rpm-ostree install kernel-tools $INSTALL_STR
+    rpm-ostree install kernel-tools "$INSTALL_STR"
     else
-    rpm-ostree install kernel-devel-matched $INSTALL_STR
+    rpm-ostree install kernel-devel-matched "$INSTALL_STR"
   fi  
 fi
