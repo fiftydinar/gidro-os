28ecfb6aabb32a8398c4df2e1e715d676cf01332
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 15:23:22 +0100

    Update akmods.sh

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -13,7 +13,6 @@ INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-
   if [[ "$IMAGE_DEVICES" == asus ]] || [ "$IMAGE_NVIDIA" == asus-nvidia ] || [ "$IMAGE_DEVICES" == surface ] || [ "$IMAGE_NVIDIA" == surface-nvidia ]; then
     rpm-ostree install kernel-tools "$INSTALL_STR"
     else
