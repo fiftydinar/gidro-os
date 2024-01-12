6418ec90c1f914572d60aaad8cd634d9b29e597a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 13:46:02 +0100

    Revert "Test akmods module pt. 2"
    This reverts commit b8ca68725773a40c305fc66d1cdab661b6696d76.

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-echo "${BASE_IMAGE}"
+IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
+IMAGE_DEVICES=$(echo "${BASE_IMAGE}" | sed 's/asus-nvidia//' | grep -o "asus" || echo "${BASE_IMAGE}" | sed 's/surface-nvidia//' | grep -o "surface")
 
 get_yaml_array INSTALL '.install[]' "$1"
 
