54b738f74d65965169163949792832a825ee622e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-14 18:04:55 +0100

    See if BASE_IMAGE variable works

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-BASED_IMAGE=$(echo "${BASE_IMAGE}")
-
 get_yaml_array INSTALL '.install[]' "$1"
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
@@ -12,7 +10,7 @@ INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-  if [[ "$BASED_IMAGE" =~ "surface" ]]; then
+  if [[ "${BASE_IMAGE}" =~ "surface" ]]; then
     rpm-ostree install kernel-surface-devel-matched $INSTALL_STR
   else  
     rpm-ostree install kernel-devel-matched $INSTALL_STR
