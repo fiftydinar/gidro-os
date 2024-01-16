a3d3bf808a848bb96b9962ec73b2f366a22da17b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-17 00:31:07 +0100

    Use rm -f flag in other scripts too

--- config/scripts/morewaita.sh
+++ config/scripts/morewaita.sh
@@ -6,7 +6,7 @@ wget https://github.com/somepaulo/MoreWaita/archive/refs/heads/main.zip -O /usr/
 cd /usr/share/icons/MoreWaita-main
 unzip main.zip -d /usr/share/icons
 find . \( -name "*.build" -o -name "*.sh" -o -name "*.md" -o -name "*.py" -o -name "*.git" \) -type f -delete
-rm -r main.zip _dev .github .gitignore
+rm -rf main.zip _dev .github .gitignore
 cd /usr/share/icons
 mv /usr/share/icons/MoreWaita-main /usr/share/icons/MoreWaita
 gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita
