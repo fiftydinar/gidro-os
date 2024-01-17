09007ab63ce59962f47f5cb9646503ac8ebf40d7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-17 11:49:25 +0100

    Use mkdir -p for system-76-scheduler config script

--- config/scripts/system76-scheduler.sh
+++ config/scripts/system76-scheduler.sh
@@ -3,5 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-mkdir /usr/etc/system76-scheduler
+mkdir -p /usr/etc/system76-scheduler
 wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
