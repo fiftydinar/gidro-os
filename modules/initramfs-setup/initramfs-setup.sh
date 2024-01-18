d53009c5da65440f13c2980fe26979680cb22299
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-18 23:32:53 +0100

    Make it easier to edit echoed user-friendly text in initramfs-setup

--- modules/initramfs-setup/initramfs-setup.sh
+++ modules/initramfs-setup/initramfs-setup.sh
@@ -11,22 +11,33 @@ set -euo pipefail
 
 get_yaml_array INCLUDE '.include[]' "$1"
 
+# If any inputted value is found, then install & configure
 if [[ ${#INCLUDE[@]} -gt 0 ]]; then
-  echo "Installing initramfs-setup"
-  printf "Configuring following initramfs files:\n%s" "${INCLUDE[@]}"
+  echo "Installing initramfs-setup"  
+  printf "Configuring following initramfs files:\n"
+  for file in "${INCLUDE[@]}"; do
+    printf "%s\n" "$file"
+  done  
+  mkdir -p /usr/etc/ublue-os/initramfs
+  
   echo "# Set to true if you want initramfs to be rebuilt again.
 # Rebuilding initramfs is useful if you are including dracut files.
 # You will see \"Updating initramfs - Manual rebuild process is running\" boot screen message after you apply this change & reboot the system.
 
-false" >> /usr/etc/ublue-os/initramfs/rebuilt
+false" > /usr/etc/ublue-os/initramfs/rebuilt
+
   echo "# This file can be modified by live-users if they want to have custom file location arguments in initramfs.
 # Be sure to check if the arguments you want already exist in initramfs by issuing \`rpm-ostree initramfs-etc\` command before modifying this file.
 # Here's an example on how to do that (ignore # symbol):
 #
 # /etc/vconsole.conf
 # /etc/crypttab
-# /etc/modprobe.d/my-modprobe.conf" >> /usr/etc/ublue-os/initramfs/tracked-custom
-  echo -e "# This file should not be modified by the user, as it's used by the OS directly.\n" >> /usr/etc/ublue-os/initramfs/tracked
-  printf "%s" "${INCLUDE[@]}" >> /usr/etc/ublue-os/initramfs/tracked
+# /etc/modprobe.d/my-modprobe.conf" > /usr/etc/ublue-os/initramfs/tracked-custom
+
+  echo -e "# This file should not be modified by the user, as it's used by the OS directly.\n" > /usr/etc/ublue-os/initramfs/tracked
+  
+  printf "%s" "${INCLUDE[@]}" >> /usr/etc/ublue-os/initramfs/tracked  
   echo "Initramfs-setup is successfully installed & configured"
+  else
+  echo "Initramfs-setup did not run installation & configuring step, be sure that values are typed correctly in recipe file"
 fi
