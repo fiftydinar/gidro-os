c1ddad771b53d7cee4ad7333d68e97966af3a778
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 11:30:48 +0100

    Fix dumb syntax error in `gschema-overrides` module
    - is not allowed in variable names, _ should be used

--- modules/gschema-overrides/gschema-overrides.sh
+++ modules/gschema-overrides/gschema-overrides.sh
@@ -4,11 +4,8 @@ set -euo pipefail
 
 get_yaml_array INCLUDE '.include[]' "$1"
 
-schema-test-location="/tmp/bluebuild-schema-test"
-schema-location="/usr/share/glib-2.0/schemas"
-
-schema-test-location || true
-schema-location || true
+schema_test_location="/tmp/bluebuild-schema-test"
+schema_location="/usr/share/glib-2.0/schemas"
 
 echo "Installing gschema-overrides module"
 
@@ -17,15 +14,15 @@ if [[ ${#INCLUDE[@]} -gt 0 ]] && [[ "${INCLUDE[@]}" == *.gschema.override ]]; th
   for file in "${INCLUDE[@]}"; do
     printf "%s\n" "$file"
   done
-  mkdir -p "$schema-test-location" "$schema-location"
-  find "$schema-location" -type f ! -name "*.gschema.override" -exec cp {} "$schema-test-location" \;
+  mkdir -p "$schema_test_location" "$schema_location"
+  find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
   for file in "${INCLUDE[@]}"; do
     cp "$schema_location/$file" "$schema_test_location"
   done
   echo "Running error test for your gschema-overrides. Aborting if failed."
-  glib-compile-schemas --strict "$schema-test-location"
+  glib-compile-schemas --strict "$schema_test_location"
   echo "Compiling gschema to include your setting overrides"
-  glib-compile-schemas "$schema-location" &>/dev/null
+  glib-compile-schemas "$schema_location" &>/dev/null
 else
   echo "Module failed because no gschema-overrides were included into the module. Please ensure that those are included properly & try again."
 fi
