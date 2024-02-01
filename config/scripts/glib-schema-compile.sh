9ed2c8b4ce5abc01561d8add3b6651998ebb1311
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 01:10:44 +0100

    Add "abort on error" support for gschema overrides in build-time

--- config/scripts/glib-schema-compile.sh
+++ config/scripts/glib-schema-compile.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
-glib-compile-schemas /usr/share/glib-2.0/schemas
+mkdir -p /tmp/gidro-schema-test
+find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/gidro-schema-test/ \;
+cp /usr/share/glib-2.0/schemas/zzz-gidro.gschema.override /tmp/gidro-schema-test/
+echo "Running error test for gidro gschema override. Aborting if failed."
+glib-compile-schemas --strict /tmp/gidro-schema-test
+echo "Compiling gschema to include gidro setting overrides"
+glib-compile-schemas /usr/share/glib-2.0/schemas &>/dev/null
+rm -rf /tmp/gidro-schema-test
