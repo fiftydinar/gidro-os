d777d3cdbcf555abc2ad1459ee3961188cc86271
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 20:13:17 +0100

    Update gschema-overrides.sh

--- modules/gschema-overrides/gschema-overrides.sh
+++ modules/gschema-overrides/gschema-overrides.sh
@@ -11,29 +11,26 @@ echo "Installing gschema-overrides module"
 
 if [[ ${#INCLUDE[@]} -gt 0 ]]; then
   for file in "${INCLUDE[@]}"; do
-  file="${file//$'\n'/}"
-  if [[ ! $file == *.gschema.override ]]; then
-    echo "Module failed because included files in module don't have .gschema.override extension."
-    exit 1
-  fi  
+    file="${file//$'\n'/}"
+    if [[ ! $file == *.gschema.override ]]; then
+      echo "Module failed because included files in module don't have .gschema.override extension."
+      exit 1
+    fi  
   done
-  else
-    printf "Applying following gschema-overrides:\n"
-    for file2 in "${INCLUDE[@]}"; do
-      file2="${file2//$'\n'/}"
-      printf "%s\n" "$file2"
-    done
-    mkdir -p "$schema_test_location" "$schema_location"
-    find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
-    for file3 in "${INCLUDE[@]}"; do
-      file_path="${schema_location}/${file3//$'\n'/}"
-      cp "$file_path" "$schema_test_location"
-    done
-    echo "Running error-test for your gschema-overrides. Aborting if test failed."
-    glib-compile-schemas --strict "$schema_test_location"
-    echo "Compiling gschema to include your setting overrides"
-    glib-compile-schemas "$schema_location" &>/dev/null
 else
-  echo "Module failed because gschema-overrides aren't included into the module."
-  exit 1
+  printf "Applying the following gschema-overrides:\n"
+  for file2 in "${INCLUDE[@]}"; do
+    file2="${file2//$'\n'/}"
+    printf "%s\n" "$file2"
+  done
+  mkdir -p "$schema_test_location" "$schema_location"
+  find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
+  for file3 in "${INCLUDE[@]}"; do
+    file_path="${schema_location}/${file3//$'\n'/}"
+    cp "$file_path" "$schema_test_location"
+  done
+  echo "Running error-test for your gschema-overrides. Aborting if the test failed."
+  glib-compile-schemas --strict "$schema_test_location"
+  echo "Compiling gschema to include your setting overrides"
+  glib-compile-schemas "$schema_location" &>/dev/null
 fi  
