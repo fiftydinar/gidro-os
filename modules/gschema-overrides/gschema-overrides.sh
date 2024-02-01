d94f145027b1afec37359d365123a411b51e4432
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 23:41:46 +0100

    Update gschema-overrides.sh

--- modules/gschema-overrides/gschema-overrides.sh
+++ modules/gschema-overrides/gschema-overrides.sh
@@ -30,7 +30,7 @@ if [[ ${#INCLUDE[@]} -gt 0 ]]; then
   done
 fi
 
-printf "Most preferred gschema-override is:\n" "%s\n" "$most_preferred_override"
+echo "Most preferred gschema-override is:\n$most_preferred_override"
 echo "If your gschema-override is not listed as most preferred, you should adjust filename prefix"
 
 # Apply gschema-override when all conditions above are satisfied
