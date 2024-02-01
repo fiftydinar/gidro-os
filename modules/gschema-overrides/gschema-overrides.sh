1f480bfedee15143c216be25e47927bc77abbb8e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 20:50:26 +0100

    Update gschema-overrides.sh

--- modules/gschema-overrides/gschema-overrides.sh
+++ modules/gschema-overrides/gschema-overrides.sh
@@ -6,32 +6,40 @@ get_yaml_array INCLUDE '.include[]' "$1"
 
 schema_test_location="/tmp/bluebuild-schema-test"
 schema_location="/usr/share/glib-2.0/schemas"
+gschema_extension=false
 
 echo "Installing gschema-overrides module"
 
+# Abort build if file in module is not included
 if [[ ${#INCLUDE[@]} == 0 ]]; then
   echo "Module failed because gschema-overrides aren't included into the module."
   exit 1
 fi
 
+# Abort build if included file does not have .gschema.override extension
 if [[ ${#INCLUDE[@]} -gt 0 ]]; then
   for file in "${INCLUDE[@]}"; do
     file="${file//$'\n'/}"
-    if [[ ! $file == *.gschema.override ]]; then
+    if [[ $file == *.gschema.override ]]; then
+      gschema_extension=true
+    else  
       echo "Module failed because included files in module don't have .gschema.override extension."
       exit 1
     fi  
   done
-else
+fi
+
+# Apply gschema-override when there files are included & when they have .gschema.override extension
+if [[ ${#INCLUDE[@]} -gt 0 ]] && $gschema_extension; then
   printf "Applying the following gschema-overrides:\n"
-  for file2 in "${INCLUDE[@]}"; do
-    file2="${file2//$'\n'/}"
-    printf "%s\n" "$file2"
+  for file in "${INCLUDE[@]}"; do
+    file="${file//$'\n'/}"
+    printf "%s\n" "$file"
   done
   mkdir -p "$schema_test_location" "$schema_location"
   find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
-  for file3 in "${INCLUDE[@]}"; do
-    file_path="${schema_location}/${file3//$'\n'/}"
+  for file in "${INCLUDE[@]}"; do
+    file_path="${schema_location}/${file//$'\n'/}"
     cp "$file_path" "$schema_test_location"
   done
   echo "Running error-test for your gschema-overrides. Aborting if the test failed."
