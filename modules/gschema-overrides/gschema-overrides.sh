3080cc936a4f05df4313df2213f7101707bfd1cb
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-01 14:33:35 +0100

    Add gschema override extension detection support

--- modules/gschema-overrides/gschema-overrides.sh
+++ modules/gschema-overrides/gschema-overrides.sh
@@ -10,20 +10,26 @@ schema_location="/usr/share/glib-2.0/schemas"
 echo "Installing gschema-overrides module"
 
 if [[ ${#INCLUDE[@]} -gt 0 ]]; then
-  printf "Applying following gschema-overrides:\n"
   for file in "${INCLUDE[@]}"; do
-    printf "%s\n" "$file"
+  if [[ $file == *.gschema.override ]]; then       
+    printf "Applying following gschema-overrides:\n"
+    for file in "${INCLUDE[@]}"; do
+      printf "%s\n" "$file"
+    done
+    mkdir -p "$schema_test_location" "$schema_location"
+    find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
+    for file in "${INCLUDE[@]}"; do
+      file_path="${schema_location}/${file//$'\n'/}"
+      cp "$file_path" "$schema_test_location"
+    done
+    echo "Running error-test for your gschema-overrides. Aborting if failed."
+    glib-compile-schemas --strict "$schema_test_location"
+    echo "Compiling gschema to include your setting overrides"
+    glib-compile-schemas "$schema_location" &>/dev/null
+  else
+    echo "Module failed because included files in module don't have .gschema.override extension."
+  fi
   done
-  mkdir -p "$schema_test_location" "$schema_location"
-  find "$schema_location" -type f ! -name "*.gschema.override" -exec cp {} "$schema_test_location" \;
-  for file in "${INCLUDE[@]}"; do
-    file_path="${schema_location}/${file//$'\n'/}"
-    cp "$file_path" "$schema_test_location"
-  done
-  echo "Running error-test for your gschema-overrides. Aborting if failed."
-  glib-compile-schemas --strict "$schema_test_location"
-  echo "Compiling gschema to include your setting overrides"
-  glib-compile-schemas "$schema_location" &>/dev/null
 else
-  echo "Module failed because no gschema-overrides were included into the module. Please ensure that those are included properly & try again."
-fi
+  echo "Module failed because no gschema-overrides are included into the module."
+fi  
