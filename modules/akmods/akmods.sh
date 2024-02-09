d515475f4631967c9ba92f8b0c71bb4a352a7ce6
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-09 19:35:30 +0100

    Update akmods.sh

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Notify that build errors can occur if upstream akmods image did not update to match latest Fedora kernel.
+# This error is printed only when build fails.
+if [ $? -eq 1 ]; then
+    echo "If you get build-time error which says: \"Could not depsolve transaction\""
+    echo "remember that issue could reside in upstream Universal Blue akmods repo"
+    echo "which can happen when akmods image is not updated to match latest Fedora kernel release."
+    echo
+    echo "Please wait for upstream to solve this issue."
+    echo "If this issue occurs for prolonged period of time, please report this issue to Universal Blue project."
+    echo "https://github.com/ublue-os/akmods"
+fi
+
 function ENABLE_MULTIMEDIA_REPO {
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
   sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
@@ -34,16 +46,4 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
     rpm-ostree install kernel-devel-matched $INSTALL_STR
     DISABLE_MULTIMEDIA_REPO
   fi  
-fi
-
-# Notify that build errors can occur if upstream akmods image did not update to match latest Fedora kernel.
-# This error is printed only when build fails.
-if [ $? -eq 1 ]; then
-    echo "If you get build-time error which says: \"Could not depsolve transaction\""
-    echo "remember that issue could reside in upstream Universal Blue akmods repo"
-    echo "which can happen when akmods image is not updated to match latest Fedora kernel release."
-    echo
-    echo "Please wait for upstream to solve this issue."
-    echo "If this issue occurs for prolonged period of time, please report this issue to Universal Blue project."
-    echo "https://github.com/ublue-os/akmods"
 fi
