20c5712e0c67e080bebd1fa4fc5f84f5fe1c4a19
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-03-15 21:30:51 +0100

    Offline ISO: Add note on why Kinoite installer variant is used

--- gidro-os-build-offline-iso.sh
+++ gidro-os-build-offline-iso.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Offline ISO size is around 4.1GB without flatpaks
+#
+# Kinoite installer variant is used because it includes option for making users
+# which is needed, because gnome-initial-setup is removed from Gidro-OS for convenience
 
 ARCH=x86_64
 VERSION=39
