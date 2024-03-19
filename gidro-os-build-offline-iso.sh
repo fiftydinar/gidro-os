14134a8b2614ede4e47e2eabc9f20cccdabf2b43
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-03-19 09:14:31 +0100

    offline-iso: Forgot to add EXTRA_BOOT_PARAMS to podman environment list

--- gidro-os-build-offline-iso.sh
+++ gidro-os-build-offline-iso.sh
@@ -20,6 +20,6 @@ ENROLLMENT_PASSWORD=ublue-os
 SECURE_BOOT_KEY_URL=https://github.com/ublue-os/akmods/raw/main/certs/public_key.der
 
 sudo podman run --rm --privileged --volume .:/isogenerator/output --security-opt label=disable --pull=newer \
--e ARCH="$ARCH" -e VERSION="$VERSION" -e IMAGE_REPO="$IMAGE_REPO" -e IMAGE_NAME="$IMAGE_NAME" -e IMAGE_TAG="$IMAGE_TAG" \
+-e ARCH="$ARCH" -e VERSION="$VERSION" -e IMAGE_REPO="$IMAGE_REPO" -e IMAGE_NAME="$IMAGE_NAME" -e IMAGE_TAG="$IMAGE_TAG" -e EXTRA_BOOT_PARAMS="$EXTRA_BOOT_PARAMS" \
 -e VARIANT="$VARIANT" -e WEB_UI="$WEB_UI" -e ENROLLMENT_PASSWORD="$ENROLLMENT_PASSWORD" -e SECURE_BOOT_KEY_URL="$SECURE_BOOT_KEY_URL" \
 ghcr.io/ublue-os/isogenerator:$VERSION
