4fb984b2e37d23f1917394b3a57f940002f5469a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-04-08 18:17:41 +0200

    Update gnome-extensions.sh

--- modules/gnome-extensions/gnome-extensions.sh
+++ modules/gnome-extensions/gnome-extensions.sh
@@ -14,35 +14,35 @@ if [[ ${#GETTEXT_DOMAIN[@]} -gt 0 ]]; then
       VERSION=$(echo "${EXTENSION}" | grep -oP 'v\d+')
       echo "Installing ${EXTENSION} Gnome extension with version ${VERSION}"
       # Download archive
-       curl -L "${URL}" --create-dirs -o "${ARCHIVE_DIR}"
-       # Extract archive
-       echo "Extracting ZIP archive"
-       unzip "${ARCHIVE_DIR}" -d "${TMP_DIR}" > /dev/null
-       # Remove archive
-       echo "Removing archive"
-       rm "${ARCHIVE_DIR}"
-       # Read necessary info from metadata.json
-       echo "Reading necessary info from metadata.json"
-       UUID=$(yq '.uuid' < "${TMP_DIR}/metadata.json")
-       SCHEMA_ID=$(yq '.settings-schema' < "${TMP_DIR}/metadata.json")
-       # Install main extension files
-       echo "Installing main extension files"
-       install -d -m 0755 "/usr/share/gnome-shell/extensions/${UUID}/"
-       find "${TMP_DIR}" -mindepth 1 -maxdepth 1 ! -path "*locale*" ! -path "*schemas*" -exec cp -r {} "/usr/share/gnome-shell/extensions/${UUID}/" \;
-       find "/usr/share/gnome-shell/extensions/${UUID}" -type d -exec chmod 0755 {} +
-       find "/usr/share/gnome-shell/extensions/${UUID}" -type f -exec chmod 0644 {} +
-       # Install schema
-       echo "Installing schema extension file"
-       install -d -m 0755 "/usr/share/glib-2.0/schemas/"
-       install -D -p -m 0644 "${TMP_DIR}/schemas/${SCHEMA_ID}.gschema.xml" "/usr/share/glib-2.0/schemas/${SCHEMA_ID}.gschema.xml"
-       # Install languages
-       echo "Installing language extension files"
-       install -d -m 0755 "/usr/share/locale/"
-       cp -r "${TMP_DIR}/locale"/* "/usr/share/locale/"
-       # Delete the temporary directory
-       echo "Cleaning up the temporary directory"
-       rm -r "${TMP_DIR}"
-       echo "------------------------------DONE----------------------------------"     
+      curl -L "${URL}" --create-dirs -o "${ARCHIVE_DIR}"
+      # Extract archive
+      echo "Extracting ZIP archive"
+      unzip "${ARCHIVE_DIR}" -d "${TMP_DIR}" > /dev/null
+      # Remove archive
+      echo "Removing archive"
+      rm "${ARCHIVE_DIR}"
+      # Read necessary info from metadata.json
+      echo "Reading necessary info from metadata.json"
+      UUID=$(yq '.uuid' < "${TMP_DIR}/metadata.json")
+      SCHEMA_ID=$(yq '.settings-schema' < "${TMP_DIR}/metadata.json")
+      # Install main extension files
+      echo "Installing main extension files"
+      install -d -m 0755 "/usr/share/gnome-shell/extensions/${UUID}/"
+      find "${TMP_DIR}" -mindepth 1 -maxdepth 1 ! -path "*locale*" ! -path "*schemas*" -exec cp -r {} "/usr/share/gnome-shell/extensions/${UUID}/" \;
+      find "/usr/share/gnome-shell/extensions/${UUID}" -type d -exec chmod 0755 {} +
+      find "/usr/share/gnome-shell/extensions/${UUID}" -type f -exec chmod 0644 {} +
+      # Install schema
+      echo "Installing schema extension file"
+      install -d -m 0755 "/usr/share/glib-2.0/schemas/"
+      install -D -p -m 0644 "${TMP_DIR}/schemas/${SCHEMA_ID}.gschema.xml" "/usr/share/glib-2.0/schemas/${SCHEMA_ID}.gschema.xml"
+      # Install languages
+      echo "Installing language extension files"
+      install -d -m 0755 "/usr/share/locale/"
+      cp -r "${TMP_DIR}/locale"/* "/usr/share/locale/"
+      # Delete the temporary directory
+      echo "Cleaning up the temporary directory"
+      rm -r "${TMP_DIR}"
+      echo "------------------------------DONE----------------------------------"     
   done
 else
   echo "ERROR: You did not specify gettext-domain"
