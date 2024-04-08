941228b7964283f7c82374121b6bff987d9d5155
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-04-08 12:46:30 +0200

    Update night-time-switcher.sh

--- config/scripts/night-time-switcher.sh
+++ config/scripts/night-time-switcher.sh
@@ -6,20 +6,42 @@ set -euo pipefail
 EXTENSION="night-time-switcher"
 if [[ "${OS_VERSION}" == "39" ]]; then
   VERSION="75"
+  echo "Installing v${VERSION} of the ${EXTENSION} extension for Fedora ${OS_VERSION}"
 elif [[ "${OS_VERSION}" == "40" ]]; then
   VERSION="77"
+  echo "Installing v${VERSION} of the ${EXTENSION} extension for Fedora ${OS_VERSION}"
 else
   echo "This extension does not support the Fedora version of your image"
   exit 1
 fi
+
+# Supported archive formats are .zip, .tar.gz, .tgz & .tar
 URL="https://gitlab.com/rmnvgr/nightthemeswitcher-gnome-shell-extension/-/releases/${VERSION}/downloads/nightthemeswitcher.zip"
 
 TMP_DIR="/tmp/${EXTENSION}"
-ZIP_DIR="${TMP_DIR}/${EXTENSION}.zip"
+ARCHIVE=$(basename "${URL}")
+ARCHIVE_DIR="${TMP_DIR}/${ARCHIVE}"
 
-curl -L "${URL}" --create-dirs -o "${ZIP_DIR}"
-unzip "${ZIP_DIR}" -d "${TMP_DIR}"
-rm "${ZIP_DIR}"
+# Download archive
+curl -L "${URL}" --create-dirs -o "${ARCHIVE_DIR}"
+
+# Extract archive
+if [[ "${ARCHIVE}" == *.zip ]]; then
+    echo "Extracting ZIP archive"
+    unzip "${ARCHIVE_DIR}" -d "${ARCHIVE_DIR}" > /dev/null
+elif [[ "${ARCHIVE}" == *.tar.gz || "${ARCHIVE_DIR}" == *.tgz ]]; then
+    echo "Extracting TAR.GZ archive"
+    tar -xzvf "${ARCHIVE_DIR}" -C "${ARCHIVE_DIR}" > /dev/null
+elif [[ "${ARCHIVE}" == *.tar ]]; then
+    echo "Extracting TAR archive"
+    tar -xvf "${ARCHIVE_DIR}" -C "${ARCHIVE_DIR}" > /dev/null
+else
+    echo "Unsupported archive format is used for extraction"
+    exit 1
+fi
+
+# Remove archive
+rm "${ARCHIVE_DIR}"
 
 UUID=$(yq '.uuid' < ${TMP_DIR}/metadata.json)
 SCHEMA_ID=$(yq '.settings-schema' < ${TMP_DIR}/metadata.json)
@@ -36,4 +58,5 @@ cp "${TMP_DIR}/schemas/${SCHEMA_ID}.gschema.xml" "/usr/share/glib-2.0/schemas/${
 mkdir -p "/usr/share/locale/"
 cp -r "${TMP_DIR}/locale"/* "/usr/share/locale/"
 
+# Delete the temporary directory
 rm -r "${TMP_DIR}"
