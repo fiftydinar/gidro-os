f4153e1414625f348d50a85716c2f1048926a5af
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-04-16 17:39:52 +0200

    Switch post-boot `initramfs-setup` module to build-time script
    This simple script basically gets rid of post-boot initramfs management, which makes things much more reliable.
    
    The only thing that is in post-boot right now is `default-flatpaks` module. That one won't switch to build-time for a long time though.
    
    I also enabled Nvidia GSP firmware for Nouveau.

---
+++ config/scripts/initramfs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rpm-ostree cliwrap install-to-root /
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
+/usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
