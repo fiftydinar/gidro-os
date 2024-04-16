d963d1479243fcebe7d6dae330805873dcf29cd2
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-04-16 18:15:35 +0200

    Update initramfs.sh

--- config/scripts/initramfs.sh
+++ config/scripts/initramfs.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-mkdir /var/tmp
+install -d -m 0755 /var/tmp
+install -d -m 0755 /root
 rpm-ostree cliwrap install-to-root /
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
