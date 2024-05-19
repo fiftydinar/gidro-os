5cbf87ab6d2aec9d791dc188be6a6338f0deec3b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-05-19 20:10:21 +0200

    Port run-every & wait-on-boot config from `chezmoi` module

--- modules/homefiles/homefiles.sh
+++ modules/homefiles/homefiles.sh
@@ -16,6 +16,8 @@ USER_CREATION_ONLY_COPY=$(echo "${1}" | yq -I=0 ".user-creation-only-copy")
 get_yaml_array USER_CREATION_ONLY_COPY_EXCEPTIONS '.user-creation-only-copy-exceptions[]' "${1}"
 DUPLICATE_FILE_POLICY=$(echo "${1}" | yq -I=0 ".duplicate-file-policy")
 get_yaml_array DUPLICATE_FILE_POLICY_EXCEPTIONS '.duplicate-file-policy-exceptions[]' "${1}"
+RUN_EVERY=$(echo "${1}" | yq -I=0 ".run-every")
+WAIT_AFTER_BOOT=$(echo "${1}" | yq -I=0 ".wait-after-boot")
 
 echo "Writing module config"
 yq -i '.user-creation-only-copy = ${USER_CREATION_ONLY_COPY}' "${HOMEFILES_CONFIG}"
@@ -28,5 +30,31 @@ yq -i '.duplicate-file-policy-exceptions[] = ${DUPLICATE_FILE_POLICY_EXCEPTIONS[
 echo "Copying homefiles"
 shopt -s dotglob
 cp -r "${CONFIG_DIRECTORY}/homefiles"/* "${HOMEFILES_BLUEBUILD}/content/"
-cp -r "${CONFIG_DIRECTORY}/homefiles"/* "$/usr/etc/skel/"
+cp -r "${CONFIG_DIRECTORY}/homefiles"/* "/usr/etc/skel/"
 shopt -u dotglob
+
+echo "Writing homefiles systemd service"
+cat >>/usr/lib/systemd/user/homefiles.service <<EOF
+[Unit]
+Description=BlueBuild homefiles update
+
+[Service]
+ExecStart=/usr/bin/homefiles
+Type=oneshot
+EOF
+
+echo "Writing homefiles systemd timer"
+cat >>/usr/lib/systemd/user/homefiles.timer <<EOF
+[Unit]
+Description=Timer for BlueBuild homefiles update
+
+[Timer]
+OnBootSec=${WAIT_AFTER_BOOT}
+OnUnitInactiveSec=${RUN_EVERY}
+
+[Install]
+WantedBy=timers.target
+EOF
+
+echo "Enabling homefiles systemd timer"
+systemctl --global enable homefiles.timer
