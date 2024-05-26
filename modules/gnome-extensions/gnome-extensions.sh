04678cb54fd29e4955a155df5b74a968518a81ea
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-05-26 21:56:25 +0200

    Implement initial installation with new method
    Also test if it works in my image with names 1st

--- modules/gnome-extensions/gnome-extensions.sh
+++ modules/gnome-extensions/gnome-extensions.sh
@@ -3,85 +3,90 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-get_yaml_array GETTEXT_DOMAIN '.install[]' "$1"
-GNOME_VER=$(gnome-shell --version | sed 's/[^0-9]*\([0-9]*\).*/\1/')
-echo "Gnome version: v${GNOME_VER}"
+get_yaml_array INSTALL '.install[]' "$1"
+get_yaml_array UNINSTALL '.uninstall[]' "$1"
 
-if [[ ${#GETTEXT_DOMAIN[@]} -gt 0 ]]; then
-  if command -v gnome-shell &> /dev/null; then 
-    for EXTENSION in "${GETTEXT_DOMAIN[@]}"; do
-        URL="https://extensions.gnome.org/extension-data/${EXTENSION}.shell-extension.zip"
-        TMP_DIR="/tmp/${EXTENSION}"
-        ARCHIVE=$(basename "${URL}")
-        ARCHIVE_DIR="${TMP_DIR}/${ARCHIVE}"
-        VERSION=$(echo "${EXTENSION}" | grep -oP 'v\d+')
-        echo "Installing ${EXTENSION} Gnome extension with version ${VERSION}"
-        # Download archive
-        wget --directory-prefix="${TMP_DIR}" "${URL}"
-        # Extract archive
-        echo "Extracting ZIP archive"
-        unzip "${ARCHIVE_DIR}" -d "${TMP_DIR}" > /dev/null
-        # Remove archive
-        echo "Removing archive"
-        rm "${ARCHIVE_DIR}"
-        # Read necessary info from metadata.json
-        echo "Reading necessary info from metadata.json"
-        EXTENSION_NAME=$(yq '.name' < "${TMP_DIR}/metadata.json")
-        UUID=$(yq '.uuid' < "${TMP_DIR}/metadata.json")
-        EXT_GNOME_VER=$(yq '.shell-version[]' < "${TMP_DIR}/metadata.json")
-        # If extension does not have the important key in metadata.json,
-        # inform the user & fail the build
-        if [[ "${UUID}" == "null" ]]; then
-          echo "ERROR: Extension '${EXTENSION_NAME}' doesn't have 'uuid' key inside metadata.json"
-          echo "You may inform the extension developer about this error, as he can fix it"
-          exit 1
-        fi
-        if [[ "${EXT_GNOME_VER}" == "null" ]]; then
-          echo "ERROR: Extension '${EXTENSION_NAME}' doesn't have 'shell-version' key inside metadata.json"
-          echo "You may inform the extension developer about this error, as he can fix it"
-          exit 1
-        fi      
-        # Compare if extension is compatible with current Gnome version
-        # If extension is not compatible, inform the user & fail the build
-        if ! [[ "${EXT_GNOME_VER}" =~ "${GNOME_VER}" ]]; then
-          echo "ERROR: Extension '${EXTENSION_NAME}' is not compatible with current Gnome v${GNOME_VER}!"
-          exit 1
-        fi  
-        # Install main extension files
-        echo "Installing main extension files"
-        install -d -m 0755 "/usr/share/gnome-shell/extensions/${UUID}/"
-        find "${TMP_DIR}" -mindepth 1 -maxdepth 1 ! -path "*locale*" ! -path "*schemas*" -exec cp -r {} "/usr/share/gnome-shell/extensions/${UUID}/" \;
-        find "/usr/share/gnome-shell/extensions/${UUID}" -type d -exec chmod 0755 {} +
-        find "/usr/share/gnome-shell/extensions/${UUID}" -type f -exec chmod 0644 {} +
-        # Install schema
-        if [[ -d "${TMP_DIR}/schemas" ]]; then
-          echo "Installing schema extension file"
-          install -d -m 0755 "/usr/share/glib-2.0/schemas/"
-          install -D -p -m 0644 "${TMP_DIR}/schemas/"*.gschema.xml "/usr/share/glib-2.0/schemas/"
-        fi  
-        # Install languages
-        # Locale is not crucial for extensions to work, as they will fallback to gschema.xml
-        # Some of them might not have any locale at the moment
-        # So that's why I made a check for directory
-        if [[ -d "${TMP_DIR}/locale" ]]; then
-          echo "Installing language extension files"
-          install -d -m 0755 "/usr/share/locale/"
-          cp -r "${TMP_DIR}/locale"/* "/usr/share/locale/"
-        fi  
-        # Delete the temporary directory
-        echo "Cleaning up the temporary directory"
-        rm -r "${TMP_DIR}"
-        echo "Extension '${EXTENSION_NAME}' is successfully installed"
-        echo "----------------------------------DONE----------------------------------"
-    done
-  else
-    echo "ERROR: Your custom image is using non-Gnome desktop environment, where Gnome extensions are not supported"
-    exit 1
-  fi  
-else
-  echo "ERROR: You did not specify the extension to install in module recipe file"
+if [[ ${#INSTALL[@]} -lt 1 ]] && [[ ${#UNINSTALL[@]} -lt 1 ]]; then
+  echo "ERROR: You did not specify the extension to install or uninstall in module recipe file"
   exit 1
 fi
+
+if ! command -v gnome-shell &> /dev/null; then 
+  echo "ERROR: Your custom image is using non-Gnome desktop environment, where Gnome extensions are not supported"
+  exit 1
+fi
+
+GNOME_VER=$(gnome-shell --version | sed 's/[^0-9]*\([0-9]*\).*/\1/')
+echo "Gnome version: ${GNOME_VER}"
+
+if [[ ${#INSTALL[@]} -gt 0 ]]; then
+  for INSTALL_EXT in "${INSTALL[@]}"; do
+      # Replaces whitespaces with %20 for install entries which contain extension name, since URLs can't contain whitespace
+      WHITESPACE_HTML=$(echo "${INSTALL_EXT}" | sed 's/ /%20/g')
+      # If only numbers are provided, than ID is inputted, otherwise, extension name
+      if [[ "${INSTALL_EXT}" =~ ^[0-9]+$ ]]; then
+        URL_QUERY=$(curl -s "https://extensions.gnome.org/extension-query/?pk=${INSTALL_EXT}&shell_version=${GNOME_VER}")
+        QUERIED_EXT=$(echo "${URL_QUERY}" | yq ".extensions[] | select(.pk == ${INSTALL_EXT})")
+      else
+        URL_QUERY=$(curl -s "https://extensions.gnome.org/extension-query/?search=${WHITESPACE_HTML}&shell_version=${GNOME_VER}")
+        QUERIED_EXT=$(echo "${URL_QUERY}" | yq ".extensions[] | select(.name == \"${INSTALL_EXT}\")")
+      fi       
+      if [[ -z "${QUERIED_EXT}" ]]; then
+        echo "Gnome extension name or UUID does not exist in https://extensions.gnome.org/ website"
+        exit 1
+      fi  
+      # Gets suitable extension version for Gnome version from the image
+      SUITABLE_VERSION=$(echo "${QUERIED_EXT}" | yq ".shell_version_map[${GNOME_VER}].version")
+      if [[ "${SUITABLE_VERSION}" == "null" ]]; then
+        echo "Gnome extension is not compatible with version of Gnome in your image"
+        exit 1
+      fi  
+      EXT_UUID=$(echo "${QUERIED_EXT}" | yq ".uuid")
+      EXT_NAME=$(echo "${QUERIED_EXT}" | yq ".name")
+      # Removes every @ symbol from UUID, since extension URL doesn't contain @ symbol
+      URL="https://extensions.gnome.org/extension-data/${EXT_UUID//@/}.v${SUITABLE_VERSION}.shell-extension.zip"
+      TMP_DIR="/tmp/${EXT_UUID}"
+      ARCHIVE=$(basename "${URL}")
+      ARCHIVE_DIR="${TMP_DIR}/${ARCHIVE}"
+      echo "Installing '${EXT_NAME}' Gnome extension with version ${SUITABLE_VERSION}"
+      # Download archive
+      wget --directory-prefix="${TMP_DIR}" "${URL}"
+      # Extract archive
+      echo "Extracting ZIP archive"
+      unzip "${ARCHIVE_DIR}" -d "${TMP_DIR}" > /dev/null
+      # Remove archive
+      echo "Removing archive"
+      rm "${ARCHIVE_DIR}"
+      # Install main extension files
+      echo "Installing main extension files"
+      install -d -m 0755 "/usr/share/gnome-shell/extensions/${EXT_UUID}/"
+      find "${TMP_DIR}" -mindepth 1 -maxdepth 1 ! -path "*locale*" ! -path "*schemas*" -exec cp -r {} "/usr/share/gnome-shell/extensions/${EXT_UUID}/" \;
+      find "/usr/share/gnome-shell/extensions/${EXT_UUID}" -type d -exec chmod 0755 {} +
+      find "/usr/share/gnome-shell/extensions/${EXT_UUID}" -type f -exec chmod 0644 {} +
+      # Install schema
+      if [[ -d "${TMP_DIR}/schemas" ]]; then
+        echo "Installing schema extension file"
+        install -d -m 0755 "/usr/share/glib-2.0/schemas/"
+        install -D -p -m 0644 "${TMP_DIR}/schemas/"*.gschema.xml "/usr/share/glib-2.0/schemas/"
+      fi  
+      # Install languages
+      # Locale is not crucial for extensions to work, as they will fallback to gschema.xml
+      # Some of them might not have any locale at the moment
+      # So that's why I made a check for directory
+      if [[ -d "${TMP_DIR}/locale" ]]; then
+        echo "Installing language extension files"
+        install -d -m 0755 "/usr/share/locale/"
+        cp -r "${TMP_DIR}/locale"/* "/usr/share/locale/"
+      fi  
+      # Delete the temporary directory
+      echo "Cleaning up the temporary directory"
+      rm -r "${TMP_DIR}"
+      echo "Extension '${EXT_NAME}' is successfully installed"
+      echo "----------------------------------DONE----------------------------------"
+  done
+fi
+
+# Uninstall section goes here
 
 # Compile gschema to include schemas from extensions
 echo "Compiling gschema to include extension schemas"
