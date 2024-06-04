90ec6de1bca957ab6847f82d7584d1525c026d68
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-06-04 17:34:36 +0200

    Add logs for removing directory in skel

--- config/scripts/fedora-skel-remover.sh
+++ config/scripts/fedora-skel-remover.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 SKEL="/usr/etc/skel"
 # Remove empty .mozilla folder
 if [[ -d "${SKEL}/.mozilla/" ]]; then
+  echo "Removing ${SKEL}/.mozilla/ directory"
   rm -r "${SKEL}/.mozilla/"
 fi
