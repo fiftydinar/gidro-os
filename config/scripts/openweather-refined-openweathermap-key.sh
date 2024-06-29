b967794781f7cb68e71596fc025d3a14169d8055
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-06-29 18:46:56 +0200

    Make OpenWeatherMap API custom key script easier to read

--- config/scripts/openweather-refined-openweathermap-key.sh
+++ config/scripts/openweather-refined-openweathermap-key.sh
@@ -3,5 +3,8 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
+DEFAULT_KEY="b4d6a638dd4af5e668ccd8574fd90cec"
+CUSTOM_KEY="d590dd084a24e33ee2198033e927543e"
+
 # Use custom OpenWeatherMap API key to solve "too many users" issue, which makes weather finally working
-sed -i 's/b4d6a638dd4af5e668ccd8574fd90cec/d590dd084a24e33ee2198033e927543e/g' /usr/share/gnome-shell/extensions/openweather-extension@penguin-teal.github.io/getweather.js
+sed -i "s/${DEFAULT_KEY}/${CUSTOM_KEY}/g" /usr/share/gnome-shell/extensions/openweather-extension@penguin-teal.github.io/getweather.js
