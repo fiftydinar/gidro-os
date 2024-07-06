91d28e04ae9639f9d539687bb2ae1d36cccccc0e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-07-06 15:37:58 +0200

    Delete redundant xdg autostart script & move initramfs script to scripts recipe

--- config/scripts/xdg-autostart-workaround.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# XDG-Autostart directory is created during first-install only
-# This can be a problem for packages like setroubleshoot to install successfully during build-time
-# So create the directory manually at the start of the build
-mkdir -p /usr/etc/xdg/autostart
