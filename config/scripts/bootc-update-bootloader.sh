3e14e3fa66bdd05dbcca37635504fc61b9f23a45
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-07-06 18:29:35 +0200

    docs: Document usage of `bootc` better

--- config/scripts/bootc-update-bootloader.sh
+++ config/scripts/bootc-update-bootloader.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-# Update bootloader inside the image
+# Update bootloader metadata inside the image, which will be utilized further by bootc in ISO to update the bootloader on system
 bootupctl backend generate-update-metadata
