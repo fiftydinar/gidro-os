e4d5838a9835562a1278fb77bc4c1a64a639b5e4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-08-04 14:53:31 +0200

    Begin

---
+++ modules/default-flatpaks/transition-legacy-to-new-format.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+# Script, which is should be sourced, if conditions are correct, for transforming existing installs config to new YAML one
