e148be809b1b1a9fb2be7a8ce29f1d97647446c0
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-10-31 07:45:21 +0100

    Remove redundant `avif` thumbnailer
    HEIF thumbnailer already caches it, see:
    https://github.com/ublue-os/main/pull/661

---
+++ files/scripts/remove-redundant-avif-thumbnailer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+# Remove avif thumbnailer, as heif thumbnailer already caches it
+rm -f "/usr/share/thumbnailers/avif.thumbnailer"
