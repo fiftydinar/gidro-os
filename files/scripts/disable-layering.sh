564a49b236b034f723fc345938ce7a2fcdd48e11
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-09 12:13:19 +0100

    Update disable-layering.sh

--- files/scripts/disable-layering.sh
+++ files/scripts/disable-layering.sh
@@ -6,29 +6,20 @@ echo "Disable rpm-ostree layering"
 sed -i '/^#*LockLayering=.*/s/.*/LockLayering=true/' /etc/rpm-ostreed.conf
 
 echo "Disable general usage of dnf & it's symlinks like yum"
-cat << EOF > /etc/profile.d/disable-layering.sh
-dnf() {
-  echo "Package/application layering is disabled in Gidro-OS to ensure that reliability & integrity of the system remains untouched,"
-  echo "Please install applications through 'Software' application only."
-  echo "Also see Gidro-OS wiki for more details about this:"
-  echo "https://github.com/fiftydinar/gidro-os/wiki/2.-Unsupported-Operations#why-rpm-ostree-installremove-or-dnf-installremove-doesnt-work-to-installremove-some-applications-"
-}
 
-dnf5() {
-  echo "Package/application layering is disabled in Gidro-OS to ensure that reliability & integrity of the system remains untouched,"
-  echo "Please install applications through 'Software' application only."
-  echo "Also see Gidro-OS wiki for more details about this:"
-  echo "https://github.com/fiftydinar/gidro-os/wiki/2.-Unsupported-Operations#why-rpm-ostree-installremove-or-dnf-installremove-doesnt-work-to-installremove-some-applications-"
-}
+package_managers=(
+"/usr/bin/dnf"
+"/usr/bin/dnf5"
+"/usr/bin/yum"
+)
 
-yum() {
-  echo "Package/application layering is disabled in Gidro-OS to ensure that reliability & integrity of the system remains untouched,"
-  echo "Please install applications through 'Software' application only."
-  echo "Also see Gidro-OS wiki for more details about this:"
-  echo "https://github.com/fiftydinar/gidro-os/wiki/2.-Unsupported-Operations#why-rpm-ostree-installremove-or-dnf-installremove-doesnt-work-to-installremove-some-applications-"
-}
+for pkg_mgr in "${package_managers[@]}"; do
+cat << EOF > "${pkg_mgr}"
+#!/usr/bin/env bash
 
-export -f dnf
-export -f dnf5
-export -f yum
+echo "Package/application layering is disabled in Gidro-OS to ensure that reliability & integrity of the system remains untouched,"
+echo "Please install applications through 'Software' application only."
+echo "Also see Gidro-OS wiki for more details about this:"
+echo "https://github.com/fiftydinar/gidro-os/wiki/2.-Unsupported-Operations#why-rpm-ostree-installremove-or-dnf-installremove-doesnt-work-to-installremove-some-applications-"
 EOF
+done
