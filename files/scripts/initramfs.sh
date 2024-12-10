4cf2d97afab98ca6b59c9e3d44499528c2ea8445
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-10 18:47:00 +0100

    Update initramfs.sh

--- files/scripts/initramfs.sh
+++ files/scripts/initramfs.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # WARNING!
-# Won't work when Fedora starts to utilize UKIs (Universal Kernel Images).
+# Won't work when Fedora starts to utilize UKIs (Unified Kernel Images).
 # UKIs contain kernel + initramfs + microcode (& maybe some other stuff)
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/bin/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
