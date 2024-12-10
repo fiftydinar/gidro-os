a9c662a36efbd6927495d6fde18fc3d3f28fb190
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-10 15:18:08 +0100

    `initramfs`: Add warning about trying to use this script on UKIs

--- files/scripts/initramfs.sh
+++ files/scripts/initramfs.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# WARNING!
+# Won't work when Fedora starts to utilize UKIs (Universal Kernel Images).
+# UKIs contain kernel + initramfs + microcode (& maybe some other stuff)
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/bin/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
 chmod 0600 "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
