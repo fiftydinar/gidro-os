dbd75bc9ec71c23e45d2f0d919a3a780ba60b154
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-13 14:09:55 +0100

    initramfs: Improve script to be more generic in determining kernel version

--- files/scripts/initramfs.sh
+++ files/scripts/initramfs.sh
@@ -5,7 +5,17 @@ set -euo pipefail
 # WARNING!
 # Won't work when Fedora starts to utilize UKIs (Unified Kernel Images).
 # UKIs contain kernel + initramfs + microcode (& maybe some other stuff)
-QUALIFIED_KERNEL="$(find "/lib/modules/" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")"
-echo "Initramfs regeneration is performing for kernel version: ${QUALIFIED_KERNEL}"
-/usr/bin/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
-chmod 0600 "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
+
+readarray -t QUALIFIED_KERNEL < <(find "/lib/modules/" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+
+if [[ "${#QUALIFIED_KERNEL[@]}" -gt 1 ]]; then
+  echo "ERROR: There are several versions of kernel's initramfs"
+  echo "       Cannot determine which one to regenerate"
+  echo "       There is a possibility that you have multiple kernels installed in the image"
+  echo "       Please only include 1 kernel in the image to solve this issue"
+  exit 1
+fi
+
+echo "Initramfs regeneration is performing for kernel version: ${QUALIFIED_KERNEL[*]}"
+/usr/bin/dracut --no-hostonly --kver "${QUALIFIED_KERNEL[*]}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL[*]}/initramfs.img"
+chmod 0600 "/lib/modules/${QUALIFIED_KERNEL[*]}/initramfs.img"
