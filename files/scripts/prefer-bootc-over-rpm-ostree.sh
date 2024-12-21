71f0554ca663f492a430a5c34a5894638d9b1403
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-21 10:40:54 +0100

    chore: Prefer `bootc` over `rpm-ostree` for update & rebase/switch command

---
+++ files/scripts/prefer-bootc-over-rpm-ostree.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cat << EOF > /etc/profile.d/prefer-bootc-over-rpm-ostree.sh
+# Prefer 'bootc update/upgrade' & 'bootc switch' over rpm-ostree's equivalent functionality
+rpm-ostree() {
+  if [[ ${#} -eq 0 ]]; then
+    /usr/bin/rpm-ostree
+  elif [[ -n "$(awk '/(^|\s)('update'|'upgrade')($|\s)/' <<< "${@}")" ]]; then
+    echo "ERROR: Don't use 'rpm-ostree update/upgrade', use 'sudo bootc update/upgrade' instead."
+    echo "       Some functionality like kargs.d is only available when using bootc, hence why Gidro-OS slowly deprecates using rpm-ostree."
+  elif [[ -n "$(awk '/(^|\s)('rebase')($|\s)/' <<< "${@}")" ]]; then
+    echo "ERROR: Don't use 'rpm-ostree rebase', use 'sudo bootc switch' instead."
+    echo "       Some functionality like kargs.d is only available when using bootc, hence why Gidro-OS slowly deprecates using rpm-ostree."
+  else
+    /usr/bin/rpm-ostree "${@}"
+  fi
+}
+
+export -f rpm-ostree
+EOF
