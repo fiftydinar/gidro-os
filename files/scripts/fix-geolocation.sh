ba018cdfa995dfa19db2303168e2aaf180fc9e33
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-02-12 22:01:25 +0100

    Put a beaconDB config file physically instead of using a script

--- files/scripts/fix-geolocation.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p /etc/geoclue/conf.d/
-echo "[wifi]
-enable=true
-url=https://api.beacondb.net/v1/geolocate" > /etc/geoclue/conf.d/99-beacondb.conf
