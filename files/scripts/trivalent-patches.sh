8fc12cde4ad77675f4c29218b3573385e624d6a3
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-02-27 16:00:33 +0100

    Disable Trivalent search engine choice pop-up
    It has a bug that it constantly reappears when Google is selected

--- files/scripts/trivalent-patches.sh
+++ files/scripts/trivalent-patches.sh
@@ -5,4 +5,7 @@ set -euo pipefail
 # Assure that network sandbox is always disabled by default (to ensure that login data remains)
 # https://github.com/fedora-silverblue/issue-tracker/issues/603
 
+# Disable search engine choice screen, as it has a bug that it constantly reappears when Google is selected
+
 echo -e '\nCHROMIUM_FLAGS+=" --disable-features=NetworkServiceSandbox"' >> /etc/trivalent/trivalent.conf
+echo -e '\nCHROMIUM_FLAGS+=" --disable-search-engine-choice-screen"' >> /etc/trivalent/trivalent.conf
