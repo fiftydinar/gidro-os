0a1d0fd53a9bf1130456bf6449ecbd86b3bb7fba
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-04-04 00:55:35 +0200

    Update trivalent-patches.sh

--- files/scripts/trivalent-patches.sh
+++ files/scripts/trivalent-patches.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 
 # Install Trivalent selinux policy (fixes Trivalent not launching since v135)
+# Prevent installing conflicting schilly
+dnf5 -y config-manager setopt fedora-cdrtools.enabled=0
+dnf5 -y config-manager setopt fedora-multimedia.enabled=0
 dnf5 -y install selinux-policy-devel
 mkdir -p ./selinux/trivalent
 cd ./selinux/trivalent
@@ -13,6 +16,8 @@ curl -fLs "${SELINUX_POLICY_URL}/trivalent.te"
 curl -sSL "${SELINUX_POLICY_URL}/trivalent.sh" | bash
 cd
 dnf5 -y remove selinux-policy-devel
+dnf5 -y config-manager setopt fedora-cdrtools.enabled=1
+dnf5 -y config-manager setopt fedora-multimedia.enabled=1
 
 # Assure that network sandbox is always disabled by default (to ensure that login data remains)
 # https://github.com/fedora-silverblue/issue-tracker/issues/603
