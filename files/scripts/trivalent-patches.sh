83d203834e7ab1e21fe444a1b9afc03dba47b538
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-04-04 01:16:28 +0200

    Update trivalent-patches.sh

--- files/scripts/trivalent-patches.sh
+++ files/scripts/trivalent-patches.sh
@@ -7,13 +7,13 @@ set -euo pipefail
 dnf5 -y config-manager setopt fedora-cdrtools.enabled=0
 dnf5 -y config-manager setopt fedora-multimedia.enabled=0
 dnf5 -y install selinux-policy-devel
-mkdir -p ./selinux/trivalent
 cd ./selinux/trivalent
 SELINUX_POLICY_URL="https://raw.githubusercontent.com/secureblue/secureblue/refs/heads/live/files/scripts/selinux/trivalent"
-curl -fLs "${SELINUX_POLICY_URL}/trivalent.fc"
-curl -fLs "${SELINUX_POLICY_URL}/trivalent.if"
-curl -fLs "${SELINUX_POLICY_URL}/trivalent.te"
-curl -sSL "${SELINUX_POLICY_URL}/trivalent.sh" | bash
+curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.fc" --output-dir "${PWD}"
+curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.if" --output-dir "${PWD}"
+curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.te" --output-dir "${PWD}"
+curl -fLs --create-dirs -O "${SELINUX_POLICY_URL}/trivalent.sh" --output-dir "${PWD}"
+bash "${PWD}/trivalent.sh"
 cd
 dnf5 -y remove selinux-policy-devel
 dnf5 -y config-manager setopt fedora-cdrtools.enabled=1
